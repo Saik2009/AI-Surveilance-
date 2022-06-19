581fe535785e6c4dfeaf9df3bf118c6c9fe34178
Saik2009 <80210806+Saik2009@users.noreply.github.com>
2022-06-19 11:21:20 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 status="";
 video="";
+object=[];
 function preload(){
 video=createVideo('video.mp4');
 video.hide();
@@ -10,6 +11,20 @@ function setup(){
 }
 function draw(){
     image(video,0,0,480,380);
+    if(status!=""){
+        cos.detect(video,gotResults);
+         for(i=0; i<=object.length;i++){
+            document.getElementById("status").innerHTML="Status:Objects Detected";
+            document.getElementById("no").innerHTML="Number of Objects:"+objects.length;
+            
+            fill("red");
+            stroke("red");
+            percentage=floor(object[i].confidence*100);
+            text(objects[i].label + " "+percentage+"%",object,objects[i].x+15,objects[i].y+15);
+            nofill();
+            rect(object[i].x,object[i].y,object[i].width,object[i].height);
+         }
+    }
 
 }
 function start(){
@@ -22,4 +37,12 @@ function modelloaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    object=results;
+    
 }
